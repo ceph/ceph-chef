40097c38d53c43bb648047b17a982b4004c81ec9
cloudm2 <cjones@cloudm2.com>
2016-07-06 08:57:56 -0400

    Updated multisite no replication

---
+++ files/ceph_cleanout_objects_from_pools_danger.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Author:: Chris Jones <cjones303@bloomberg.net>
+#
+# Copyright 2016, Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# IMPORTANT: DANGER - DANGER - DANGER
+# Can only be ran from a Ceph node
+
+for pool in $(rados lspools); do
+  for obj in $(rados -p $pool ls); do
+    echo "Removing $obj from pool $pool"
+    rados -p $pool rm $obj
+  done
+done
